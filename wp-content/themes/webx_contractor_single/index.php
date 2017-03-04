325ab61e14f123813ff270964cc4079c530a1811
Zach <zrubinrattet@gmail.com>
2017-03-03 18:54:56 -0800

    modified index.php to be a bit cleaner in it's approach

--- wp-content/themes/webx_contractor_single/index.php
+++ wp-content/themes/webx_contractor_single/index.php
@@ -1,20 +1,20 @@
 <?php get_header(); 
 
 $sections = array(
-	'company-toggle',
-	'gallery-toggle',
-	'services-toggle',
-	'menu-toggle',
-	'blog-toggle',
-	'areas-served-toggle',
-	'testimonials-toggle',
-	'coupons-toggle',
-	'contact-toggle',
+	'company',
+	'gallery',
+	'services',
+	'menu',
+	'blog',
+	'areas-served',
+	'testimonials',
+	'coupons',
+	'contact',
 );
 
 foreach($sections as $section){
-	if(get_field($section, 'option')){
-		include(locate_template( 'modules/' . str_replace('-toggle', '', $section) . '.php' ));
+	if(get_field($section . '-toggle', 'option')){
+		include(locate_template( 'modules/' . $section . '.php' ));
 	}
 }
 
