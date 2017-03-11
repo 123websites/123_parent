dc861e6e0ffa6ef1475765d6ce8aaa9d075df700
Zach <zrubinrattet@gmail.com>
2017-03-10 17:19:13 -0800

    areas served in a good place

--- wp-content/themes/webx_contractor_single/modules/areas-served.php
+++ wp-content/themes/webx_contractor_single/modules/areas-served.php
@@ -1,7 +1,8 @@
-<main class="areas-served" id="areas-served">
+<?php $has_bg = !empty(get_field('areas-served-bg', 'option')); ?>
+<main class="areas-served main<?php echo $has_bg ? ' main--hasbg' : ''; ?>" id="areas-served" <?php if($has_bg): ?> style="background-image: url('<?php echo get_field('areas-served-bg', 'option'); ?>');" <?php endif; ?>>
 	<section class="areas-served-hero hero"">
 		<div class="areas-served-hero-text hero-text">
-			<h1 class="areas-served-hero-text-header hero-text-header">areas served</h1>
+			<h1 class="areas-served-hero-text-header hero-text-header<?php echo !$has_bg ? ' hero-text-header--nobg' : ''; ?>"><?php echo get_field('areas-served-alt-toggle', 'option') ? get_field('areas-served-alt', 'option') : 'areas served' ?></h1>
 		</div>
 		<div class="areas-served-hero-tint hero-tint"></div>
 		<div class="areas-served-hero-map" id="map"></div>
@@ -11,25 +12,14 @@
 			<div class="areas-served-areas-grid">
 				<?php 
 				$rows = get_field('locations', 'option'); 
-				$medium_remainder = count($rows) % 2;
-				$large_remainder = count($rows) % 4;
 				foreach($rows as $index => $row): 
-					$classes = '';
-					if(!empty($medium_remainder) && $index >= count($rows) - $medium_remainder){
-						$classes .= 'medium-' . (string) $medium_remainder . ' ';
-					}
-					if(!empty($large_remainder) && $index >= count($rows) - $large_remainder){
-						$classes .= 'large-' . (string) $large_remainder . ' ';
-					}
-					?>
-					<?php $contents = simplexml_load_file('http://maps.googleapis.com/maps/api/geocode/xml?address='.$row['zip'].'@&sensor=true'); ?>
-					<a href="https://www.google.com/maps/@<?php echo $contents->result->geometry->location->lat . ',' . $contents->result->geometry->location->lng . ',14z'; ?>" class="areas-served-areas-grid-imagecontainer<?php echo !empty($classes) ? ' ' . $classes : '';  ?>" target="_blank">
+					$contents = simplexml_load_file('http://maps.googleapis.com/maps/api/geocode/xml?address='.$row['zip'].'@&sensor=true'); ?>
+					<a href="https://www.google.com/maps/@<?php echo $contents->result->geometry->location->lat . ',' . $contents->result->geometry->location->lng . ',14z'; ?>" class="areas-served-areas-grid-imagecontainer" target="_blank">
+						<div style="background-image: url('<?php echo $row['area-image']; ?>');" class="areas-served-areas-grid-imagecontainer-image"></div>
 						<div class="areas-served-areas-grid-imagecontainer-citystate"><?php 
 							preg_match_all('/^.*?(?=(\d))/', $contents->result->formatted_address, $preg_match_all_matches);
 							echo $preg_match_all_matches[0][0];
 						?></div>
-						<div class="areas-served-areas-grid-imagecontainer-tint"></div>
-						<div style="background-image: url('<?php echo $row['area-image']; ?>');" class="areas-served-areas-grid-imagecontainer-image"></div>
 					</a>
 			<?php endforeach; ?>
 			</div>
