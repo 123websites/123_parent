86369dcc563649a441412809a211a03dfb158ae5
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-07-31 00:58:21 -0700

    used the dashboard ad on mobile in the theme settings

--- wp-content/themes/123_parent/build/js/admin.js
+++ wp-content/themes/123_parent/build/js/admin.js
@@ -8,9 +8,20 @@ var Admin = {};
 			_init: function(){
 				if( $('body').is('.toplevel_page_general-settings') ){
 					$('#side-sortables').append(
+						'<style>' +
+							'@media only screen{' +
+								'.training-ad-desktop{ display: none; width: 100%;}' +
+								'.training-ad-mobile{ display: block; width: 100%;}' +
+							'}' +
+							'@media only screen and (min-width: 851px){' +
+								'.training-ad-desktop{ display: block;}' +
+								'.training-ad-mobile{ display: none;}' +
+							'}' +
+						'</style>' +
 						'<div class="postbox">' +
 							'<a target="_blank" href="http://www.123websites.com/training">' +
-								'<img style="width: 100%;" src="http://123websites.com/images/training-ad.png"/>' +
+								'<img class="training-ad-desktop" src="http://123websites.com/images/training-ad.png"/>' +
+								'<img class="training-ad-mobile" src="http://123websites.com/images/training-ad-dashboard.png"/>' +
 							'</a>' +
 						'</div>'
 					);
