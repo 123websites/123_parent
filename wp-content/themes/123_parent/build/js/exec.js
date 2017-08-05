d07dd1580322f50ea2c0954b6688e88022af2311
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-08-04 17:21:57 -0700

    forgot to add the exec.js files

---
+++ wp-content/themes/123_parent/build/js/exec.js
@@ -0,0 +1,31 @@
+;(function ( $, Theme, window, document, undefined ) {
+
+	$(document).ready(function(){
+
+		Theme.FadeEffects._init();
+
+		Theme.Galleries._init();
+
+		Theme.HeroSlider._init();
+
+		Theme.MobileNav._init();
+
+		Theme.DesktopNav._init();
+
+		Theme.AnimateNavScrolling._init();
+		
+		Theme.HomeTestimonialsSlider._init();
+		
+		Theme.Estimate._init();
+
+		Theme.PA._init();
+
+		Theme.CookieMonster._init();
+
+		Theme.MenuGrid._init();
+
+		Theme.Parallax._init();
+
+	});
+
+})( jQuery, Theme, window, document );
