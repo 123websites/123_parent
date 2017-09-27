ac118159cef9c8f1abf3564f42135471a5fde85b
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-09-26 18:01:48 -0700

    updated head link to custom/custom.css

--- wp-content/themes/123_parent/header.php
+++ wp-content/themes/123_parent/header.php
@@ -21,7 +21,7 @@
 		<?php wp_head(); ?>		
 		<?php echo !empty(get_field('custom-css', 'option')) ? '<style type="text/css">' . get_field('custom-css', 'option') . '</style>' : ''; ?>
 		<?php include( locate_template( 'partials/color-pickers.php' ) ); ?>
-		<?php if( file_exists( get_template_directory() . '/build/css/custom.css' ) ): ?>
+		<?php if( file_exists( get_template_directory() . '/build/css/custom/custom.css' ) ): ?>
 			<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/build/css/custom/custom.css">
 		<?php endif; ?>
 		<!--[if lt IE 9]>
