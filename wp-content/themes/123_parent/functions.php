10693d58b2389cea42b5f18f1526be44dc419afc
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-09-26 21:53:19 -0700

    updated functions.php still testing wppusher

--- wp-content/themes/123_parent/functions.php
+++ wp-content/themes/123_parent/functions.php
@@ -1225,7 +1225,6 @@ add_action( 'after_setup_theme', 'action_create_custom_css' );
 
 if( !function_exists('action_create_custom_css') ){
 	function action_create_custom_css(){
-		var_dump( !file_exists( get_template_directory() . '/build/css/custom/custom.css' ) );
 		if( !file_exists( get_template_directory() . '/build/css/custom/custom.css' ) ){
 			touch( get_template_directory() . '/build/css/custom/custom.css' );
 			chmod( get_template_directory() . '/build/css/custom/custom.css', 0755 );
