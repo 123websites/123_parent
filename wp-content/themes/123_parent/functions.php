81f8eb8bb0fc16eb99cbcd85f6c1d21077f5997c
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-10-01 09:59:29 -0700

    testing file_get_contents

--- wp-content/themes/123_parent/functions.php
+++ wp-content/themes/123_parent/functions.php
@@ -1282,8 +1282,8 @@ if( !function_exists('check_section_sort_exists') ){
 }
 
 
+$contents = file_get_contents('https://raw.githubusercontent.com/bahamas10/css-color-names/master/css-color-names.json');
 
-
-
+var_dump($contents);
 
 ?>
