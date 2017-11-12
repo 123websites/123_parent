1d9dc0ac9005614bec721cf29d4c678f5065f04a
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-11-11 23:36:42 -0800

    small refactoring of data in handler.php fusiontables

--- wp-content/themes/123_parent/fusiontables/handler.php
+++ wp-content/themes/123_parent/fusiontables/handler.php
@@ -1,12 +1,29 @@
 <?php 
 
 class FusionTableHandler{
-	
+	private $table_data = array(
+		array(
+			'name' => 'states',
+			'pair_delimiter' => ',0.0 ',
+			'geometry_index' => 2,
+		),
+		array(
+			'name' => 'counties',
+			'pair_delimiter' => ' ',
+			'geometry_index' => 4,
+		),
+		array(
+			'name' => 'countries',
+			'pair_delimiter' => ',0',
+			'geometry_index' => 0,
+		),
+	);
+
 	private $table_names = ['states', 'counties', 'countries'];
 
 	private $pair_delimiters = [',0.0 ', ' ', ',0'];
 
-	private $geometry_locations = [2, 4, 0];
+	private $geometry_indexes = [2, 4, 0];
 
 	private $fusion_table_columns = array(
 		'states' => ['name', 'id', 'geometry'],
@@ -15,6 +32,7 @@ class FusionTableHandler{
 	);
 
 	private $sql_table_names;
+
 	private $table_prefix;
 
 	public function __construct(){
@@ -22,17 +40,89 @@ class FusionTableHandler{
 		$this->table_prefix = $wpdb->prefix . '123ft_';
 
 		$this->sql_table_names = array_map(function($el){
-			return $this->table_prefix . $el;
-		}, $this->table_names);
+			return $this->table_prefix . $el['name'];
+		}, $this->table_data);
 	}
 	public function build_sql_tables(){
 		foreach($this->sql_table_names as $index => $sql_table_name){
 			if( $this->sql_table_exists($sql_table_name) == false ){
 				$this->create_sql_table($sql_table_name);
-				$fusion_table_data = $this->get_fusion_table_data($this->table_names[$index], $this->pair_delimiters[$index], $this->geometry_locations[$index]);
+				$fusion_table_data = $this->get_fusion_table_data($this->table_data[$index]['name']);
 				$this->insert_fusion_table_data($sql_table_name, $fusion_table_data);
 			}
 		}
+	}
+	public function get_fusion_table_data($table_name){
+		// setup internal vars
+		$geometry_index;
+		$pair_delimiter;
+		foreach($this->table_data as $el){
+			if( $el['name'] == $table_name ){
+				$geometry_index = $el['geometry_index'];
+				$pair_delimiter = $el['pair_delimiter'];
+			}
+		}
+
+		// get lines
+		$csv = [];
+		$lines = file(get_template_directory_uri() . '/fusiontables/' . $table_name . '.csv', FILE_IGNORE_NEW_LINES);
+		foreach ($lines as $key => $value) {
+		    $csv[$key] = str_getcsv($value);
+		}
+
+		$contents_arr = [];
+		
+		// parse rows	
+		foreach($csv as $index => $content){
+			// ignore first row
+			if( $index == 0 ){
+				continue;
+			}
+			else{
+				// skip empty rows or rows without geometry
+				if( !empty($content) ){
+					if( !isset($content[$geometry_index]) ){
+						continue;
+					}
+
+					// create xml element
+					$xml = new SimpleXMLElement($content[$geometry_index]);
+
+					// get all the coordinates elements
+					$coords_xml = $xml->xpath('//coordinates');
+
+					// every polygon coordinate element
+					$all_coord_pairs = [];
+					foreach($coords_xml as $coords_el){
+						// convert xml el to string
+						$coords_el_str = $coords_el->__toString();
+						// apply pair separation delimeter
+						$comma_separated_coordinate_pairs = explode($pair_delimiter, $coords_el_str);
+						// every coordinate pair - comma separated
+						$coord_pairs_array = [];
+						foreach($comma_separated_coordinate_pairs as $el){
+							$el_explode = explode(',', str_replace(' ', '', $el));
+							
+							//build coordinate pair latlng if a complete latlng is available
+							if( isset($el_explode[1]) ){
+								$coord_pairs_array[] = array(
+									'lat' => $el_explode[1],
+									'lng' => $el_explode[0]
+								);
+							}
+						}
+						// add to all coordinate pairs for this polygon coordinate element
+						$all_coord_pairs[] = $coord_pairs_array;
+					}
+					// replace the kml geometry with the arrays of coordinate pairs
+					$content[$geometry_index] = $all_coord_pairs;
+
+					// build array of rows from csv
+					$contents_arr[] = $content;
+				}
+			}
+		}
+		return $contents_arr;
 	}
 	private function insert_fusion_table_data($sql_table_name, $fusion_table_data){
 		global $wpdb;
@@ -71,72 +161,10 @@ class FusionTableHandler{
 		$rows = $wpdb->get_results('SHOW TABLES LIKE "' . $sql_table_name . '"');
 		return !empty($rows);
 	}
-	public function get_fusion_table_data($table_name, $pair_delimiter, $geometry_location){
-		// get lines
-		$csv = [];
-		$lines = file(get_template_directory_uri() . '/fusiontables/' . $table_name . '.csv', FILE_IGNORE_NEW_LINES);
-		foreach ($lines as $key => $value) {
-		    $csv[$key] = str_getcsv($value);
-		}
-
-		$contents_arr = [];
-		
-		// parse rows	
-		foreach($csv as $index => $content){
-			// ignore first row
-			if( $index == 0 ){
-				continue;
-			}
-			else{
-				// skip empty rows or rows without geometry
-				if( !empty($content) ){
-					if( !isset($content[$geometry_location]) ){
-						continue;
-					}
-
-					// create xml element
-					$xml = new SimpleXMLElement($content[$geometry_location]);
-
-					// get all the coordinates elements
-					$coords_xml = $xml->xpath('//coordinates');
-
-					// every polygon coordinate element
-					$all_coord_pairs = [];
-					foreach($coords_xml as $coords_el){
-						// convert xml el to string
-						$coords_el_str = $coords_el->__toString();
-						// apply pair separation delimeter
-						$comma_separated_coordinate_pairs = explode($pair_delimiter, $coords_el_str);
-						// every coordinate pair - comma separated
-						$coord_pairs_array = [];
-						foreach($comma_separated_coordinate_pairs as $el){
-							$el_explode = explode(',', str_replace(' ', '', $el));
-							
-							//build coordinate pair latlng if a complete latlng is available
-							if( isset($el_explode[1]) ){
-								$coord_pairs_array[] = array(
-									'lat' => $el_explode[1],
-									'lng' => $el_explode[0]
-								);
-							}
-						}
-						// add to all coordinate pairs for this polygon coordinate element
-						$all_coord_pairs[] = $coord_pairs_array;
-					}
-					// replace the kml geometry with the arrays of coordinate pairs
-					$content[$geometry_location] = $all_coord_pairs;
-
-					// build array of rows from csv
-					$contents_arr[] = $content;
-				}
-			}
-		}
-		return $contents_arr;
-	}
 }
 
 $fth = new FusionTableHandler();
-// error_log(print_r($fth->get_fusion_table_data('countries', ',0', 0), true));
+
 $fth->build_sql_tables();
 
 ?>
